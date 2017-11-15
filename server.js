79a54ed28bf56c6dd3d2f355cca75a0593d8f9f9
Emily <eplummer26@gmail.com>
2017-11-14 20:13:42 -0800

    set up routes and server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app, {});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
